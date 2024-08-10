2e202445efc5de9efc546fec68eba27c02fcf80e
CakesTwix <oleg.kiryazov@gmail.com>
2024-08-10 16:05:01 +0300

    Some fix and another stuff

--- repo/plugin.video.eneyida/test.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-r = requests.post("https://eneyida.tv/index.php?do=search", data={"do": "search", "subaction": "search", "story": "Пошук"})
-
-soup = BeautifulSoup(r.content, 'html.parser')
-
-for item in soup.find_all("article", class_="short"):
-    print(item.find("a", class_="short_title").text)
